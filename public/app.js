57f010c3340d5ac1e959ff2f04d442527d260753
Alex Hudson <ahudson@alexhudsondesign.com>
2019-07-09 15:19:55 -0700

    incremental scaffolding setup

--- public/app.js
+++ public/app.js
@@ -1,1 +1,6 @@
-console.log('hello world');
+console.log(d3);
+
+var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+svg.setAttribute('id', 'foo');
+svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
+document.body.appendChild(svg);
