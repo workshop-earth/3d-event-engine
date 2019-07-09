2ceecdfab2120ef2b864e7b91fe8fa09db1ffdea
Alex Hudson <ahudson@alexhudsondesign.com>
2019-07-09 11:12:25 -0700

    Added boilerplate gulp pipeline

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+'use strict';
+const   gulp = require('gulp');
+
+
+gulp.task('js', function(){
+    gulp.src('src/app.js')
+    .pipe(gulp.dest('public'));
+});
+
+gulp.task('build', ['js'], function(){
+    console.log('build command');
+    return
+});
+
+gulp.task('default', ['build']);
+
+
+
