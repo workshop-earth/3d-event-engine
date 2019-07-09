58e55d7e43eabf0b55d913f9c398aa01e30ff90d
Alex Hudson <ahudson@alexhudsondesign.com>
2019-07-09 16:48:46 -0700

    updated js

--- public/app.js
+++ public/app.js
@@ -1,4 +1,7 @@
+// var data = require('./data/data.json');
+
 console.log(d3);
+// console.log(data);
 
 var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 svg.setAttribute('id', 'foo');
