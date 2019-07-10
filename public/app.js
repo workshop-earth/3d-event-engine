5d5b676f009b3b654c8ddb04f65b58422ec9a441
Alex Hudson <ahudson@alexhudsondesign.com>
2019-07-10 14:11:48 -0700

    Fixed axes; added scales; added resize

--- public/app.js
+++ public/app.js
@@ -1,10 +1,10 @@
 var	vizHolder = document.querySelector('#vizHolder'),
-		data,
 		request = new XMLHttpRequest(),
 		datapath = '../data/data.json',
-		padding = 20,
-		height = (vizHolder.offsetHeight - (padding * 2)),
-		width = (vizHolder.offsetWidth - (padding * 2));
+		padding = 40,
+		height,
+		width,
+		data;
 
 //Data fetch
 request.open('GET', datapath, true);
@@ -18,21 +18,55 @@ request.onload = function() {
 request.onerror = function() { console.log('There was a connection error of some sort'); };
 request.send();
 
+
+function debounce(func, wait, immediate) {
+	let timeout;
+	return function() {
+		const context = this, args = arguments;
+		const later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		const callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
 function init() {
-	var n = data.dataset.length;
+	drawViz();
+
+	window.addEventListener('resize', debounce( function(){
+		drawViz();
+	} ), 250);
+}
+
+function drawViz() {
+	//Configure size, dump any existing SVG/elements (mainly for resize function)
+	height = vizHolder.offsetHeight;
+	width = vizHolder.offsetWidth;
+	d3.select(vizHolder).selectAll("*").remove();
+
+	//Draw it all
+	var n = d3.max(d3.values(data.dataset)) + 10;
 
 	var xScale = d3.scaleLinear()
-		.domain([0, n-1])
-		.range([0, width - padding*2]);
+		.domain([0, n])
+		.range([0, width - (padding * 2)]);
 
 	var yScale = d3.scaleLinear()
-		.domain([0, 1])
-		.range([height-padding, 0]);
+		.domain([0, n])
+		.range([height - (padding * 2), 0]);
+
+	var rScale = d3.scaleLinear()
+		.domain([0, n])
+		.range([5, 40]);
 
 	var svg = d3.select(vizHolder)
 				.append('svg')
-				.attr('height', height + padding)
-				.attr('width', width + padding);
+				.attr('height', height)
+				.attr('width', width);
 
 	var viz = svg.append('g')
 				.attr('id', 'viz')
@@ -40,23 +74,24 @@ function init() {
 
 	viz.append("g")
 		.attr("class", "x axis")
-		.attr("transform", "translate(0," + (height-padding) + ")")
+		.attr("transform", "translate(0," + (height - (padding * 2)) + ")")
 		.call(d3.axisBottom(xScale));
 
 	viz.append("g")
 		.attr("class", "y axis")
-		.attr("transform", "translate(" + padding + ", 0)")
 		.call(d3.axisLeft(yScale));
 
 	var circles = viz.selectAll("circle")
 		.data(data.dataset)
 		.enter()
 		.append("circle")
-		.attr('r', 10)
+		.attr('r', function(d, i){
+			return rScale(d)
+		})
 		.attr('cx', function(d, i){
-			return i*15
+			return xScale(d)
 		})
 		.attr('cy', function(d, i){
-			return i*15
+			return yScale(d)
 		});
 }
