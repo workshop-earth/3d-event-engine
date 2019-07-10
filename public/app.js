aab7d50c32051fa2991eca4e0f024adad977873d
Alex Hudson <ahudson@alexhudsondesign.com>
2019-07-10 11:15:57 -0700

    Updated build system; added Sass

--- public/app.js
+++ public/app.js
@@ -1,9 +1,10 @@
+var visHolder = document.querySelector('#vizHolder');
 // var data = require('./data/data.json');
 
 console.log(d3);
 // console.log(data);
 
 var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-svg.setAttribute('id', 'foo');
+svg.setAttribute('id', 'viz');
 svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
-document.body.appendChild(svg);
+visHolder.appendChild(svg);
