2f98583e896135fc63e20b37f0b95b693dce3cde
Alex Hudson <ahudson@alexhudsondesign.com>
2019-07-10 11:15:57 -0700

    Updated build system; added Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,33 @@
 'use strict';
-const   gulp = require('gulp');
+const	gulp	= require('gulp'),
+			sass	= require('gulp-sass');
 
 
 gulp.task('js', function(){
-    gulp.src('src/app.js')
+    gulp.src('src/js/app.js')
     .pipe(gulp.dest('public'));
 });
 
-gulp.task('watch', function(){
-    gulp.watch('src/app.js', ['js']);
+
+gulp.task('css', function(){
+    return gulp.src('src/scss/style.scss')
+        .pipe(sass({
+										outputStyle: 'compressed',
+										includePaths: [
+											'./node_modules/reset-css/sass/'
+										]
+									})
+            .on('error', sass.logError))
+        .pipe(gulp.dest('public'));
 });
 
-gulp.task('build', ['js'], function(){
-    console.log('build command');
+gulp.task('watch', function(){
+    gulp.watch('src/js/app.js', ['js']);
+    gulp.watch('src/scss/style.scss', ['css']);
+});
+
+gulp.task('build', ['js', 'css'], function(){
+    console.log('Building site...');
     return
 });
 
