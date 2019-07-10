448e1f84c559f7bb08f897de53537741e92c7964
Alex Hudson <ahudson@alexhudsondesign.com>
2019-07-10 11:36:10 -0700

    Added placeholder data call

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,21 @@
-var visHolder = document.querySelector('#vizHolder');
-// var data = require('./data/data.json');
+var	visHolder = document.querySelector('#vizHolder'),
+		data,
+		request = new XMLHttpRequest(),
+		datapath = '../data/data.json';
 
-console.log(d3);
-// console.log(data);
+//Data fetch
+request.open('GET', datapath, true);
+request.onload = function() {
+	if (request.status >= 200 && request.status < 400) {
+		console.log('Data received');
+		data = JSON.parse(request.responseText);
+	} else { console.log('Reached our target server, but it returned an error'); }
+};
+request.onerror = function() { console.log('There was a connection error of some sort'); };
+request.send();
 
+
+//Init
 var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
 svg.setAttribute('id', 'viz');
 svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
